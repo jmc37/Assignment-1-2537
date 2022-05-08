450af66c40de9f0a58bbf6f0dd8da1f06ca06d24
Joseph Chan <josephchan36@gmail.com>
2022-05-08 16:00:31 -0700

    update

--- server.js
+++ server.js
@@ -1,7 +1,4 @@
 const port = process.env.PORT || 5000;
-app.listen(PORT, () => {
-    console.log(`Our app is running on port ${ PORT }`);
-});
 const express = require('express')
 const app = express()
 app.set('view engine', 'ejs');
