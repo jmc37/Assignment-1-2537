ef3d8b604900e502e9e0d2690ae8deb0489a53f8
Joseph Chan <josephchan36@gmail.com>
2022-05-25 21:37:43 -0700

    updated game

--- public/scripts/game.js
+++ public/scripts/game.js
@@ -2,14 +2,23 @@ hasflippedcard = false
 firstCard = undefined
 secondCard = undefined
 
-function setup(){
-    $('.card').on('click', function (){
-        $(this).toggleClass("flip");
 
-        if(!hasFlippedCard){
-            firstCard = $(this).find('.front_face')[0]
+$(".card").on('click', function () {
+    $(this).toggleClass("flip");
 
-            
-        }
-    })
-}
+    // if (!hasFlippedCard) {
+    //     firstCard = $(this).find('.front_face')[0]
+    //     console.log(firstCard)
+    //     hasFlippedCard = true;
+    // } else {
+    //     secondCard = $(this).find('.front_face')[0]
+    // }
+    // if (
+    //     $(`#$(firstCard.id)`).attr("src") ==
+    //     $(`#$(secondCard.id)`).attr("src")
+    // ) {
+    //     console.log('a match!');
+    // } else {
+    //     console.log('not a match!');
+    // }
+})
