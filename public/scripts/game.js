8e5eee5e22966d0d3f930dd0e7c1172821882f05
Joseph Chan <josephchan36@gmail.com>
2022-05-28 11:25:20 -0700

    base game works

--- public/scripts/game.js
+++ public/scripts/game.js
@@ -3,30 +3,79 @@ firstCard = undefined
 secondCard = undefined
 lockboard = false;
 poke_img = []
+win = 0
+pokemon_total = 3
 card = ''
 
-function add_img(data) {
-        poke_img.push(data.sprites.other["official-artwork"].front_default)
-        poke_img.push(data.sprites.other["official-artwork"].front_default)
 
-console.log(poke_img)
+
+function game() {
+    console.log('called')
+    if (lockboard) {
+        return
+    } else {
+        $(this).addClass('flip')
+        if (!hasflippedcard) {
+            hasflippedcard = true;
+            firstCard = $(this).find('.front_face')[0]
+            console.log(firstCard)
+        } else {
+            hasflippedcard = false;
+            secondCard = $(this).find('.front_face')[0]
+            console.log(firstCard, secondCard)
+        }
+        if (secondCard) {
+            if (  $(`#${firstCard.id}`).attr("src") == $(`#${secondCard.id}`).attr("src")) {
+                console.log("a  match");
+                $(`#${firstCard.id}`).parent().off("click")
+                $(`#${secondCard.id}`).parent().off("click")
+                firstCard = undefined
+                secondCard = undefined
+                win += 1
+                if (win == pokemon_total){
+                    alert("You win!")
+                }
+            } else {
+                console.log('not a match')
+                setTimeout(() => {
+                    $(`#${firstCard.id}`).parent().removeClass("flip");
+                    
+
+                    $(`#${secondCard.id}`).parent().removeClass("flip")
+                    firstCard = undefined
+                    secondCard = undefined
+                }, 1000)
+                
+            }
+        }
+    }
 }
 
-function cards(){
-    for (i = 0; i <= 5; i++){
+
+
+function add_img(data) {
+    poke_img.push(data.sprites.other["official-artwork"].front_default)
+    poke_img.push(data.sprites.other["official-artwork"].front_default)
+    console.log(poke_img)
+}
+
+function cards() {
+    for (i = 0; i <= 5; i++) {
         card += `
-        <div class="card">
-        <img id="img${i}" class="front_face" src="${poke_img[i]}" alt="">
-        <img class="back_face" src="../images/pokemon.png" alt="">
-    </div>
-        `
+    <div class="card">
+    <img id="img${i}" class="front_face" src="${poke_img[i]}" alt="">
+    <img class="back_face" src="../images/pokemon.png" alt="">
+</div>
+    `
     }
     console.log(card)
     $("#game_grid").html(card)
 
 }
 
-
+function start_game(){
+    console.log('game started')
+}
 // Display images
 async function loadImages() {
     for (i = 1; i <= 3; i++) { // three times
@@ -41,53 +90,10 @@ async function loadImages() {
     cards();
 }
 
-
-
- function setup() {
+function setup() {
     loadImages();
-    $(".card").on('click', function () {
-        console.log('called')
-        if (lockboard)
-            return
-        $(this).addClass('flip')
-        if (!hasflippedcard) {
-            hasflippedcard = true;
-            firstCard = $(this).find('.front_face')[0]
-            console.log(firstCard)
-        } else {
-            hasflippedcard = false;
+    $("body").on("click", ".card", game)
 
-            // if ((firstCard) == $(this).find('.front_face')[0]) {
-            //     $(`#${firstCard.id}`).parent().removeClass("flip")
-            //     firstCard = undefined
-            //     secondCard = undefined
-            //     return
-            // }
 
-            secondCard = $(this).find('.front_face')[0]
-            console.log(firstCard, secondCard)
-        }
-        if (secondCard)
-            if (
-                $(`#${firstCard.id}`).attr("src") ==
-                $(`#${secondCard.id}`).attr("src")
-            ) {
-                console.log("a  match");
-                $(`#${firstCard.id}`).parent().off("click")
-                $(`#${secondCard.id}`).parent().off("click")
-                firstCard = undefined
-                secondCard = undefined
-            }
-        else {
-            console.log('not a match')
-            lockboard = true;
-            setTimeout(() => {
-                $(`#${firstCard.id}`).parent().removeClass("flip");
-
-                $(`#${secondCard.id}`).parent().removeClass("flip")
-                lockboard = false;
-            }, 1000)
-        }
-    })
 }
 $(document).ready(setup)
